973443eaec649dd38e9bcead3c7ed75fbaa82b64
Shahriar Mohammad <shahriar@binate-solutions.com>
2019-10-28 17:21:19 +0600

    super user creation, email validity added.

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -6,8 +6,8 @@ class ModelTests(TestCase):
 
     def test_create_user_with_email_successful(self):
         """Test creating a new user with an email successful"""
-        email = "shams@shams.com"
-        password = "Test1234"
+        email = 'shams@shams.com'
+        password = 'Test1234'
         user = get_user_model().objects.create_user(
             email=email,
             password=password
@@ -15,3 +15,28 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test the email of the user is normalized"""
+        email = 'shams@SHAMSU.com'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password='pasword1234',
+        )
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """Test creating user with no email raises error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_superuser(None, "pass1234")
+
+    def test_create_new_superuser(self):
+        """Test creating new super user"""
+        user = get_user_model().objects.create_superuser(
+            email='shams@SHAMSU.com',
+            password='pasword1234',
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
